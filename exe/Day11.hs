3e1fafbc6b890c980a3755fec081379f1125494c
Tom Cumming <mail@tomcumming.co.uk>
2023-12-11 11:17:53 +0000

    Day 11 part 2

--- exe/Day11.hs
+++ exe/Day11.hs
@@ -1,28 +1,30 @@
 module Main (main) where
 
 import AOC.IO ()
-import Control.Arrow (second)
 import Control.Category ((>>>))
 import Data.List (transpose)
+import System.Environment (getArgs)
 import System.IO (getContents')
 
 parseInput :: String -> [String]
 parseInput = lines
 
-extraRows :: [[Char]] -> [[Char]]
-extraRows = expand >>> transpose >>> expand >>> transpose
- where
-  expand = \case
-    [] -> []
-    x : xs | all (== '.') x -> x : x : expand xs
-    x : xs -> x : expand xs
+yCoords :: Int -> Int -> [[Char]] -> [[(Char, Int)]]
+yCoords gap n = \case
+  [] -> []
+  l : ls | all (== '.') l -> yCoords gap (n + gap) ls
+  l : ls -> fmap (,n) l : yCoords gap (succ n) ls
 
-starCoords :: [[Char]] -> [(Int, Int)]
-starCoords =
-  fmap (`zip` [0 :: Int ..])
-    >>> zipWith (\y -> fmap (second (,y))) [0 :: Int ..]
-    >>> concat
-    >>> concatMap (\(c, p) -> [p | c == '#'])
+xCoords :: Int -> Int -> [[(Char, Int)]] -> [[(Int, Int)]]
+xCoords gap n = \case
+  [] -> []
+  l : ls | all (fst >>> (== '.')) l -> xCoords gap (n + gap) ls
+  l : ls ->
+    concatMap (\(c, y) -> [(n, y) | c == '#']) l
+      : xCoords gap (succ n) ls
+
+coords :: Int -> [[Char]] -> [(Int, Int)]
+coords gap = yCoords gap 0 >>> transpose >>> xCoords gap 0 >>> concat
 
 distances :: [(Int, Int)] -> [Int]
 distances = \case
@@ -32,11 +34,15 @@ distances = \case
   go (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
 
 main :: IO ()
-main =
+main = do
+  gap <-
+    getArgs >>= \case
+      [] -> pure 2
+      ["part2"] -> pure 1000000
+      _ -> fail "Unexpected args"
   getContents'
     >>= ( parseInput
-            >>> extraRows
-            >>> starCoords
+            >>> coords gap
             >>> distances
             >>> sum
             >>> print
