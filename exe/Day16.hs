92a3f4c396615d07d128796c810be86a50e964c7
Tom Cumming <mail@tomcumming.co.uk>
2023-12-16 12:10:17 +0000

    Day 16 part 2

--- exe/Day16.hs
+++ exe/Day16.hs
@@ -1,9 +1,12 @@
 module Main (main) where
 
 import Control.Category ((>>>))
+import Data.Foldable (fold, maximumBy)
 import Data.Function ((&))
 import Data.Map qualified as M
+import Data.Ord (comparing)
 import Data.Set qualified as S
+import System.Environment (getArgs)
 import System.IO (getContents')
 
 data Tile = Floor | SplitV | SplitH | RightUp | RightDown deriving (Show)
@@ -84,12 +87,35 @@ stepper fls vs cs = case step fls vs cs of
   cs' | S.null cs' -> vs <> cs
   cs' -> stepper fls (vs `S.union` cs) cs'
 
+edgeStarts :: M.Map Coord Tile -> [(Coord, Dir)]
+edgeStarts flr =
+  fold
+    [ [((x, 0), Down) | x <- [0 .. maxX]]
+    , [((x, maxY), Up) | x <- [0 .. maxX]]
+    , [((0, y), Right_) | y <- [0 .. maxY]]
+    , [((maxX, y), Left_) | y <- [0 .. maxY]]
+    ]
+ where
+  maxX = M.keys flr & fmap fst & maximum
+  maxY = M.keys flr & fmap snd & maximum
+
+part1 :: M.Map Coord Tile -> S.Set Coord
+part1 fls =
+  stepper fls mempty (S.singleton ((0, 0), Right_))
+    & foldMap (fst >>> S.singleton)
+
+part2 :: M.Map Coord Tile -> S.Set Coord
+part2 fls = maximumBy (comparing S.size) $ do
+  start <- edgeStarts fls
+  stepper fls mempty (S.singleton start) & foldMap (fst >>> S.singleton) & pure
+
 main :: IO ()
-main =
+main = do
+  f <-
+    getArgs >>= \case
+      [] -> pure part1
+      ["part2"] -> pure part2
+      _ -> fail "Unexpected args"
   getContents'
     >>= parseInput
-    >>= (floorGrid >>> go >>> S.size >>> print)
- where
-  go fls =
-    stepper fls mempty (S.singleton ((0, 0), Right_))
-      & foldMap (fst >>> S.singleton)
+    >>= (floorGrid >>> f >>> S.size >>> print)
