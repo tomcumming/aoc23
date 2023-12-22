f9f2bea489ea59b8de6051f64cd262d9b33079b8
Tom Cumming <mail@tomcumming.co.uk>
2023-12-22 17:42:33 +0000

    Day 22 part 2 (slow brute)

--- exe/Day22.hs
+++ exe/Day22.hs
@@ -3,13 +3,12 @@ module Main (main) where
 import AOC.IO (readFail)
 import AOC.List (split)
 import Control.Category ((>>>))
-import Control.Monad (guard)
 import Control.Monad.State (State, evalState, gets, modify)
+import Data.Bool (bool)
 import Data.Function ((&))
 import Data.Functor ((<&>))
 import Data.Map qualified as M
-import Data.Maybe (fromMaybe)
-import Data.Set qualified as S
+import System.Environment (getArgs)
 import System.IO (getContents')
 
 type Coord = (Int, Int, Int)
@@ -45,11 +44,6 @@ overlapHoriz ((x11, y11, _), (x12, y12, _)) ((x21, y21, _), (x22, y22, _)) =
 under :: Brick -> Brick -> Bool
 under b1@((_, _, z11), _) b2@(_, (_, _, z22)) = overlapHoriz b1 b2 && z11 > z22
 
-over :: Brick -> Brick -> Bool
-over b1@((_, _, z11), _) b2@(_, (_, _, z22)) =
-  overlapHoriz b1 b2
-    && z11 == succ z22
-
 topOf :: Brick -> Int
 topOf ((_, _, _), (_, _, z2)) = z2
 
@@ -57,14 +51,14 @@ setFloor :: Brick -> Int -> Brick
 setFloor ((x1, y1, z1), (x2, y2, z2)) flr =
   ((x1, y1, flr), (x2, y2, flr + (z2 - z1)))
 
-settle :: [Brick] -> Brick -> State (M.Map Brick Brick) Brick
-settle bs b = do
+settle' :: [Brick] -> Brick -> State (M.Map Brick Brick) Brick
+settle' bs b = do
   existing <- gets (M.lookup b)
   case existing of
     Just b' -> pure b'
     Nothing -> do
       let us = filter (under b) bs
-      us' <- traverse (settle bs) us
+      us' <- traverse (settle' bs) us
       let top = case us' of
             [] -> 0
             _ -> us' <&> topOf & maximum
@@ -72,33 +66,31 @@ settle bs b = do
       modify (M.insert b b')
       pure b'
 
-disolvable :: [Brick] -> [Brick]
-disolvable bs =
-  let bs' = evalState (traverse (settle bs) bs) mempty
-      os =
-        ( do
-            b1 <- bs'
-            b2 <- bs'
-            over b1 b2 & guard
-            pure (b1, S.singleton b2)
-        )
-          & M.fromListWith (<>)
-      us =
-        M.toList os
-          & foldMap (\(b1, bs2) -> S.toList bs2 <&> (,S.singleton b1))
-          & M.fromListWith (<>)
-   in do
-        me <- bs'
-        let overMe = M.lookup me us & maybe [] S.toList
-        let actuallyDependant =
-              filter
-                ((`M.lookup` os) >>> fromMaybe mempty >>> S.size >>> (== 1))
-                overMe
-        null actuallyDependant & guard
-        pure me
+settle :: [Brick] -> [Brick]
+settle bs = evalState (traverse (settle' bs) bs) mempty
+
+withoutOne :: [a] -> [[a]]
+withoutOne = go []
+ where
+  go xs = \case
+    [] -> []
+    (x : ys) -> (xs <> ys) : go (x : xs) ys
+
+movers :: [Brick] -> [Int]
+movers bs = do
+  bs' <- withoutOne bs
+  zipWith (==) bs' (settle bs')
+    <&> bool 1 0
+    & sum
+    & pure
 
 main :: IO ()
-main =
+main = do
+  f <-
+    getArgs >>= \case
+      [] -> pure (filter (== 0) >>> length)
+      ["part2"] -> pure sum
+      _ -> fail "Unexpected args"
   getContents'
     >>= parseInput
-    >>= (disolvable >>> length >>> print)
+    >>= (settle >>> movers >>> f >>> print)
